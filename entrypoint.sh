12e8c6a0799d5e74f2b156cd74d90f550f94a061
Marcus Noble <github@marcusnoble.co.uk>
2024-05-09 11:35:55 +0100

    Improvements to entrypoint.sh
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
 set -e
+set -o pipefail
 
 cd /app
 
 SUITES_TO_RUN=$(find $1 -name '*_suite_test.go' | xargs dirname | uniq | xargs)
 shift
 
+echo "About to run the following test suites: ${SUITES_TO_RUN}"
+
 REPORT_DIR=${REPORT_DIR:-/tmp/reports}
 mkdir -p ${REPORT_DIR}
+echo "Test results will be saved to: ${REPORT_DIR}"
 
+echo "🛠️ Building test suites... (this may take a short while with no log output)"
 ginkgo --output-dir=${REPORT_DIR} --junit-report=test-results.xml --timeout 4h --keep-going -v -r $@ ${SUITES_TO_RUN}
